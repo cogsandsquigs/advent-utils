366fc9fe7f47dada6a3dcb0041d27c93d6fa5975
Ian Pratt <ianjdpratt@gmail.com>
2022-12-14 20:11:09 -0600

    changes ig

--- src/grid.rs
+++ src/grid.rs
@@ -1,30 +1,57 @@
 use crate::point::Point;
+use std::ops::{Index, IndexMut};
 
 /// A matrix of values. The matrix is stored in row-major order.
 pub struct Grid<T> {
     /// The width of the matrix.
-    width: usize,
+    pub width: usize,
     /// The height of the matrix.
-    height: usize,
+    pub height: usize,
     /// The values of the matrix.
     values: Vec<T>,
 }
 
 /// Public API for Grid.
 impl<T> Grid<T> {
+    /// Returns a new `Grid` with the given width and height. The values of the matrix are
+    /// initialized with the default value of `T`.
+    pub fn new(width: usize, height: usize) -> Self
+    where
+        T: Default + Clone,
+    {
+        Self {
+            width,
+            height,
+            values: vec![T::default(); width * height],
+        }
+    }
+
+    /// Returns a new `Grid` with the given width and height. The values of the matrix are
+    /// initialized with the given value.
+    pub fn new_with_value(width: usize, height: usize, value: T) -> Self
+    where
+        T: Clone,
+    {
+        Self {
+            width,
+            height,
+            values: vec![value; width * height],
+        }
+    }
+
     /// Returns the value at the given coordinates.
     pub fn get(&self, point: Point) -> &T {
-        &self.values[point.y * self.width + point.x]
+        &self.values[point.y as usize * self.width + point.x as usize]
     }
 
     /// Returns a mutable reference to the value at the given coordinates.
     pub fn get_mut(&mut self, point: Point) -> &mut T {
-        &mut self.values[point.y * self.width + point.x]
+        &mut self.values[point.y as usize * self.width + point.x as usize]
     }
 
     /// Returns the value at the given coordinates.
     pub fn set(&mut self, point: Point, value: T) {
-        self.values[point.y * self.width + point.x] = value;
+        self.values[point.y as usize * self.width + point.x as usize] = value;
     }
 
     /// Returns the set of all neighbors of the given coordinates. The neighbors are returned in the
@@ -39,49 +66,42 @@ impl<T> Grid<T> {
 
     /// Returns the set of all neighbors orthogonal to the given coordinates.
     /// The neighbors are returned in the order of left, right, up, down.
+    /// TODO: Change points to be i32 instead of usize.
     pub fn orthogonal_neighbors(&self, point: Point) -> Vec<Point> {
-        let mut neighbors: Vec<Point> = vec![];
-
-        if point.x > 0 {
-            neighbors.push(Point::new(point.x - 1, point.y));
-        }
-
-        if point.x < self.width - 1 {
-            neighbors.push(Point::new(point.x + 1, point.y));
-        }
-
-        if point.y > 0 {
-            neighbors.push(Point::new(point.x, point.y - 1));
-        }
-
-        if point.y < self.height - 1 {
-            neighbors.push(Point::new(point.x, point.y + 1));
-        }
-
-        neighbors
+        #[allow(clippy::absurd_extreme_comparisons)]
+        #[allow(unused_comparisons)]
+        point
+            .orthogonal_neighbors()
+            .into_iter()
+            .filter(|p| p.x >= 0 && p.x < self.width as i32 && p.y >= 0 && p.y < self.height as i32)
+            .collect()
     }
 
     /// Returns the set of all neighbors diagonal to the given coordinates.
     /// The neighbors are returned in the order of top-left, top-right, bottom-left, bottom-right.
     pub fn diagonal_neighbors(&self, point: Point) -> Vec<Point> {
-        let mut neighbors: Vec<Point> = vec![];
+        #[allow(clippy::absurd_extreme_comparisons)]
+        #[allow(unused_comparisons)]
+        point
+            .diagonal_neighbors()
+            .into_iter()
+            .filter(|p| p.x >= 0 && p.x < self.width as i32 && p.y >= 0 && p.y < self.height as i32)
+            .collect()
+    }
+}
 
-        if point.x > 0 && point.y > 0 {
-            neighbors.push(Point::new(point.x - 1, point.y - 1));
-        }
+/// Indexing implementation for Grid.
+impl<T> Index<Point> for Grid<T> {
+    type Output = T;
 
-        if point.x < self.width - 1 && point.y > 0 {
-            neighbors.push(Point::new(point.x + 1, point.y - 1));
-        }
+    fn index(&self, point: Point) -> &Self::Output {
+        self.get(point)
+    }
+}
 
-        if point.x > 0 && point.y < self.height - 1 {
-            neighbors.push(Point::new(point.x - 1, point.y + 1));
-        }
-
-        if point.x < self.width - 1 && point.y < self.height - 1 {
-            neighbors.push(Point::new(point.x + 1, point.y + 1));
-        }
-
-        neighbors
+/// Mutable indexing implementation for Grid.
+impl<T> IndexMut<Point> for Grid<T> {
+    fn index_mut(&mut self, point: Point) -> &mut Self::Output {
+        self.get_mut(point)
     }
 }
--- src/point.rs
+++ src/point.rs
@@ -1,28 +1,29 @@
+/// A point in a 2D grid.
+/// TODO: Change points to be i32 instead of usize.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Point {
-    pub x: usize,
-    pub y: usize,
+    pub x: i32,
+    pub y: i32,
 }
 
 /// Public API for Point.
 impl Point {
     /// Returns a new `Point`.
-    pub fn new(x: usize, y: usize) -> Self {
+    pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
 
     /// Computes the Manhattan distance between two points.
-    pub fn manhattan_distance(&self, other: &Self) -> usize {
-        (self.x as isize - other.x as isize).unsigned_abs()
-            + (self.y as isize - other.y as isize).unsigned_abs()
+    pub fn manhattan_distance(&self, other: &Self) -> u32 {
+        (self.x - other.x).unsigned_abs() + (self.y - other.y).unsigned_abs()
     }
 
     /// Computes the set of all integer-valued points between two points.
     pub fn line(&self, other: &Self) -> Vec<Self> {
         let mut points = vec![];
 
-        let x_diff = (other.x as isize - self.x as isize).unsigned_abs();
-        let y_diff = (other.y as isize - self.y as isize).unsigned_abs();
+        let x_diff = (other.x - self.x).unsigned_abs();
+        let y_diff = (other.y - self.y).unsigned_abs();
 
         let x_step = if self.x < other.x { 1 } else { -1 };
         let y_step = if self.y < other.y { 1 } else { -1 };
@@ -30,12 +31,50 @@ impl Point {
         for i in 0..=x_diff {
             for j in 0..=y_diff {
                 points.push(Self::new(
-                    (self.x as isize + i as isize * x_step) as usize,
-                    (self.y as isize + j as isize * y_step) as usize,
+                    (self.x + i as i32 * x_step),
+                    (self.y + j as i32 * y_step),
                 ));
             }
         }
 
         points
+    }
+
+    /// Returns the set of all neighbors of the given coordinates. The neighbors are returned in the
+    /// order of left, right, up, down, top-left, top-right, bottom-left, bottom-right.
+    pub fn neighbors(&self) -> Vec<Self> {
+        [self.orthogonal_neighbors(), self.diagonal_neighbors()].concat()
+    }
+
+    /// Returns the set of all neighbors orthogonal to the given coordinates.
+    /// The neighbors are returned in the order of left, right, up, down.
+    pub fn orthogonal_neighbors(&self) -> Vec<Self> {
+        let mut neighbors: Vec<Self> = vec![];
+
+        neighbors.push(Self::new(self.x - 1, self.y));
+
+        neighbors.push(Self::new(self.x + 1, self.y));
+
+        neighbors.push(Self::new(self.x, self.y - 1));
+
+        neighbors.push(Self::new(self.x, self.y + 1));
+
+        neighbors
+    }
+
+    /// Returns the set of all neighbors diagonal to the given coordinates.
+    /// The neighbors are returned in the order of top-left, top-right, bottom-left, bottom-right.
+    pub fn diagonal_neighbors(&self) -> Vec<Self> {
+        let mut neighbors: Vec<Self> = vec![];
+
+        neighbors.push(Self::new(self.x - 1, self.y - 1));
+
+        neighbors.push(Self::new(self.x + 1, self.y - 1));
+
+        neighbors.push(Self::new(self.x - 1, self.y + 1));
+
+        neighbors.push(Self::new(self.x + 1, self.y + 1));
+
+        neighbors
     }
 }
