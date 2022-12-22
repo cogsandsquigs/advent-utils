0c903f6e2b5f55a6a8155dc32be3d9afb731c8ca
Ian Pratt <ianjdpratt@gmail.com>
2022-12-21 20:18:39 -0600

    changed stuffs

--- src/files.rs
+++ src/files.rs
@@ -1,19 +1,22 @@
+use anyhow::{anyhow, Result};
 use core::fmt::Debug;
-use std::{
-    fs::File,
-    io::{Read, Result},
-    path::Path,
-};
+use std::{fmt::Display, fs::File, io::Read, path::Path};
 
 /// Read the contents of a file into a `String`. Takes in anything that is a valid `Path` and can be
 /// cloned, which includes `&str`, `String`, `PathBuf`, etc.
 pub fn read<P>(path: P) -> Result<String>
 where
-    P: AsRef<Path> + Debug + Clone,
+    P: AsRef<Path> + Debug + Clone + Display,
 {
     let mut contents = String::new();
 
-    File::open(path)?.read_to_string(&mut contents)?;
+    let mut file = match File::open(path.clone()) {
+        Ok(file) => Ok(file),
+        // TODO: only catch file not found errors here and bubble up others
+        Err(..) => Err(anyhow!("File not found: {}", path)),
+    }?;
+
+    file.read_to_string(&mut contents)?;
 
     Ok(contents)
 }
