67c0217311aff5c72b8b2185b05868b0b29563c0
Ian Pratt <ianjdpratt@gmail.com>
2023-12-03 16:09:56 -0600

    basf

--- advent-utils-macros/src/lib.rs
+++ advent-utils-macros/src/lib.rs
@@ -51,7 +51,6 @@ pub fn solution(args: TokenStream, item: TokenStream) -> TokenStream {
             let result = #inner_fn_name(&input);
             let elapsed = start.elapsed();
 
-            
             println!("Day {}, part {} solution: {}", #day, #part, result);
             println!(
                 "Time elapsed: {}s {}ms {}µs {}ns",
